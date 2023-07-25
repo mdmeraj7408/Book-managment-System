1fec063f54e9b696effa2fc22a5f241d511a2be3
meraj7408 <108111906+meraj7408@users.noreply.github.com>
2023-07-26 00:26:10 +0530

    by @meraj alam

---
+++ src/test/java/com/merajgenius/bookmanagmentsystem/BookManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merajgenius.bookmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
